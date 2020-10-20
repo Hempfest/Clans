1fe127e70fb89793b9b1587fbe61941edd2ed0c8
BuildTools <unconfigured@null.spigotmc.org>
2020-10-19 23:08:38 -0700

    Optimized non clan players

--- src/main/java/com/youtube/hempfest/clans/util/events/PlayerKillPlayerEvent.java
+++ src/main/java/com/youtube/hempfest/clans/util/events/PlayerKillPlayerEvent.java
@@ -76,7 +76,6 @@ public class PlayerKillPlayerEvent extends Event{
         }
         if (clanUtil.getClan(killer) == null) {
             if (clanUtil.getClan(victim) != null) {
-                if (!clanUtil.getClan(killer).equals(clanUtil.getClan(victim))) {
                     Clan dead = getClan(clanUtil.getClan(victim), victim);
                     dead.takePower(0.11);
                     DataManager dm2 = new DataManager(victim.getUniqueId().toString(), null);
@@ -84,7 +83,6 @@ public class PlayerKillPlayerEvent extends Event{
                     int deaths = user2.getConfig().getInt("deaths");
                     user2.getConfig().set("deaths", (deaths + 1));
                     user2.saveConfig();
-                }
             }
         }
     }
